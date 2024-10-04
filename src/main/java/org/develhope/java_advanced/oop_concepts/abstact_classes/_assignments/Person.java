117bec8412be91f97711f1fab26084a38561ffa7
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-10-04 16:05:22 +0300

    Instructions

--- src/main/java/org/develhope/java_advanced/oop_concepts/abstact_classes/_assignments/Person.java
+++ src/main/java/org/develhope/java_advanced/oop_concepts/abstact_classes/_assignments/Person.java
@@ -1,6 +1,18 @@
 package org.develhope.java_advanced.oop_concepts.abstact_classes._assignments;
 
 abstract class Person {
+    /*
+         2- Create inheritance chain.
+
+            Create abstract Person Class and inherit that by Man class than inherit that Man by WorkerMan.
+            Inherit Person by Woman then inherit Women by WomanWorker.
+
+            Each class should have function: printNameAndType
+
+            Create main function, in the main function create 6-7-8 people(each of them will be type of Man or WorkerMan or Woman or WorkerWoman) (edited)
+            [15:36]
+
+     */
     String name;
     int age;
 
