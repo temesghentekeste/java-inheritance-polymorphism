5dc9f38d094d364e0cf5d03482049efaec3348cd
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-10-04 15:08:47 +0300

    Calculator interface

---
+++ src/main/java/org/develhope/java_advanced/oop_concepts/interfaces/_assignments/Calculator.java
@@ -0,0 +1,17 @@
+package org.develhope.java_advanced.oop_concepts.interfaces._assignments;
+
+public interface Calculator {
+    /*
+        1- Create 'Calculator' interface. in this interface there should be 4 functions: Sum, Subtract, Multiply, Divide.
+            implement this interface from 'CalculatorClass' and implement necessary functions.
+
+            All the classes and interface and main function should be in separate files.
+            NEW
+            [15:05]
+            Deadline : 3.25pm
+     */
+    double sum(double a, double b);
+    double subtract(double a, double b);
+    double multiply(double a, double b);
+    double divide(double a, double b);
+}
