121ff0e7bef0b88d3c4029e85af7e4f23aee08a8
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-10-07 16:32:30 +0300

    TwoDoorBMW,  TwoDoorFord, TwoDoorHyundai classes, inherited from TwoDoorCar

---
+++ src/main/java/org/develhope/java_advanced/oop_concepts/abstact_classes/_assignments/vehicle/TwoDoorFord.java
@@ -0,0 +1,41 @@
+package org.develhope.java_advanced.oop_concepts.abstact_classes._assignments.vehicle;
+
+public class TwoDoorFord extends TwoDoorCar {
+
+    public TwoDoorFord(String model, int year) {
+        super("Ford", model, year);
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("Ford engine started.");
+    }
+
+    @Override
+    public void stopEngine() {
+        System.out.println("Ford engine stopped.");
+    }
+
+    @Override
+    public void openDoors() {
+        System.out.println("Opening all four Ford doors.");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Ford is running.");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Ford is stopping");
+    }
+
+    @Override
+    public void repair() {
+        System.out.println("Ford is being repaired");
+
+    }
+}
+
+
---
+++ src/main/java/org/develhope/java_advanced/oop_concepts/abstact_classes/_assignments/vehicle/TwoDoorHyundai.java
@@ -0,0 +1,41 @@
+package org.develhope.java_advanced.oop_concepts.abstact_classes._assignments.vehicle;
+
+public class TwoDoorHyundai extends TwoDoorCar {
+
+    public TwoDoorHyundai(String model, int year) {
+        super("Hyundai", model, year);
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("Hyundai engine started.");
+    }
+
+    @Override
+    public void stopEngine() {
+        System.out.println("Hyundai engine stopped.");
+    }
+
+    @Override
+    public void openDoors() {
+        System.out.println("Opening all four Hyundai doors.");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hyundai is running.");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Hyundai is stopping");
+    }
+
+    @Override
+    public void repair() {
+        System.out.println("Hyundai is being repaired");
+
+    }
+}
+
+
